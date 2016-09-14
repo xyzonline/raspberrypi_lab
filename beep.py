fb24962e410c8987e3fa8dd32fe530e5981e59a6
wwj718 <wuwenjie718@gmail.com>
2016-09-14 20:42:17 +0800

    Create beep.py

---
+++ beep.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+#蜂鸣器
+import RPi.GPIO as GPIO   #import the GPIO library
+import time               #import the time library
+
+
+# 葫芦娃
+NOTE_D0=-1
+NOTE_D1=294
+NOTE_D2=330
+NOTE_D3=350
+NOTE_D4=393
+NOTE_D5=441
+NOTE_D6=495
+NOTE_D7=556
+
+NOTE_DL1=147
+NOTE_DL2=165
+NOTE_DL3=175
+NOTE_DL4=196
+NOTE_DL5=221
+NOTE_DL6=248
+NOTE_DL7=278
+
+NOTE_DH1=589
+NOTE_DH2=661
+NOTE_DH3=700
+NOTE_DH4=786
+NOTE_DH5=882
+NOTE_DH6=990
+NOTE_DH7=1112
+#以上部分是定义是把每个音符和频率值对应起来，其实不用打这么多，但是都打上了，后面可以随意编写D调的各种歌，我这里用NOTE_D+数字表示音符，NOTE_DH+数字表示上面有点的那种音符，NOTE_DL+数字表示下面有点的那种音符。这样后面写起来比较好识别。
+
+WHOLE=1
+HALF=0.5
+QUARTER=0.25
+EIGHTH = 0.25
+SIXTEENTH=0.625
+#这部分是用英文对应了拍子，这样后面也比较好看
+
+# 曲调
+hlw_tune =[
+  NOTE_DH1,NOTE_D6,NOTE_D5,NOTE_D6,NOTE_D0,
+  NOTE_DH1,NOTE_D6,NOTE_D5,NOTE_DH1,NOTE_D6,NOTE_D0,NOTE_D6,
+  NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D6,NOTE_D0,NOTE_D6,
+  NOTE_DH1,NOTE_D6,NOTE_D5,NOTE_DH1,NOTE_D6,NOTE_D0,
+
+  NOTE_D1,NOTE_D1,NOTE_D3,
+  NOTE_D1,NOTE_D1,NOTE_D3,NOTE_D0,
+  NOTE_D6,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D6,
+  NOTE_D5,NOTE_D1,NOTE_D3,NOTE_D0,
+  NOTE_DH1,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D6,
+  NOTE_D5,NOTE_D1,NOTE_D2,NOTE_D0,
+  NOTE_D7,NOTE_D7,NOTE_D5,NOTE_D3,
+  NOTE_D5,
+  NOTE_DH1,NOTE_D0,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_D0,NOTE_D5,NOTE_D1,NOTE_D3,NOTE_D0,
+  NOTE_DH1,NOTE_D0,NOTE_D6,NOTE_D6,NOTE_D5,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_D0,NOTE_D5,NOTE_D1,NOTE_D2,NOTE_D0,
+  NOTE_D3,NOTE_D3,NOTE_D1,NOTE_DL6,
+  NOTE_D1,
+  NOTE_D3,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_D3,NOTE_D5,NOTE_D6,NOTE_D6,
+  NOTE_DH1,NOTE_D0,NOTE_D7,NOTE_D5,
+  NOTE_D6,
+]
+
+hlw_duration = [
+  1,1,0.5,0.5,1,
+  0.5,0.5,0.5,0.5,1,0.5,0.5,
+  0.5,1,0.5,1,0.5,0.5,
+  0.5,0.5,0.5,0.5,1,1,
+
+  1,1,1+1,
+  0.5,1,1+0.5,1,
+  1,1,0.5,0.5,1,
+  0.5,1,1+0.5,1,
+  0.5,0.5,0.5,0.5,1+1,
+  0.5,1,1+0.5,1,
+  1+1,0.5,0.5,1,
+  1+1+1+1,
+  0.5,0.5,0.5+0.25,0.25,0.5+0.25,0.25,0.5+0.25,0.25,
+  0.5,1,0.5,1,1,
+  0.5,0.5,0.5+0.25,0.25,0.5+0.25,0.25,0.5+0.25,0.25,
+  0.5,1,0.5,1,1,
+  1+1,0.5,0.5,1,
+  1+1+1+1,
+  0.5,1,0.5,1+1,
+  0.5,1,0.5,1+1,
+  1+1,0.5,0.5,1,
+  1+1+1+1
+]
+
+
+class Buzzer(object):
+ def __init__(self):
+  GPIO.setmode(GPIO.BCM)
+  self.buzzer_pin = 25 #set to GPIO pin 5
+  GPIO.setup(self.buzzer_pin, GPIO.IN)
+  GPIO.setup(self.buzzer_pin, GPIO.OUT)
+  print("buzzer ready")
+
+ def __del__(self):
+  class_name = self.__class__.__name__
+  print (class_name, "finished")
+
+ def buzz(self,pitch, duration):   #create the function “buzz” and feed it the pitch and duration)
+
+  if(pitch==0):
+   time.sleep(duration)
+   return
+  period = 1.0 / pitch     #in physics, the period (sec/cyc) is the inverse of the frequency (cyc/sec)
+  delay = period / 2     #calcuate the time for half of the wave
+  cycles = int(duration * pitch)   #the number of waves to produce is the duration times the frequency
+
+  for i in range(cycles):    #start a loop from 0 to the variable “cycles” calculated above
+   GPIO.output(self.buzzer_pin, True)   #set pin 18 to high
+   time.sleep(delay)    #wait with pin 18 high
+   GPIO.output(self.buzzer_pin, False)    #set pin 18 to low
+   time.sleep(delay)    #wait with pin 18 low
+
+ def play(self, tune):
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(self.buzzer_pin, GPIO.OUT)
+  x=0
+
+  print("Playing tune ",tune)
+  if(tune==1):
+    #pitches=[262,294,330,349,392,440,494,523, 587, 659,698,784,880,988,1047]
+    pitches =hlw_tune
+    print(pitches)
+    #duration=0.1
+    duration=hlw_duration
+    for i in range(len(pitches)):
+      self.buzz(pitches[i], 0.4*duration[i])  #feed the pitch and duration to the function, “buzz”
+      #time.sleep(0.1*0.4)
+    #for p in reversed(pitches):
+    #  self.buzz(p, duration)
+    #  time.sleep(duration *0.5)
+  GPIO.setup(self.buzzer_pin, GPIO.IN)
+  '''
+  elif(tune==2):
+    pitches=[262,330,392,523,1047]
+    duration=[0.2,0.2,0.2,0.2,0.2,0,5]
+    for p in pitches:
+      self.buzz(p, duration[x])  #feed the pitch and duration to the function, “buzz”
+      time.sleep(duration[x] *0.5)
+      x+=1
+  elif(tune==3):
+    pitches=[392,294,0,392,294,0,392,0,392,392,392,0,1047,262]
+    duration=[0.2,0.2,0.2,0.2,0.2,0.2,0.1,0.1,0.1,0.1,0.1,0.1,0.8,0.4]
+    for p in pitches:
+      self.buzz(p, duration[x])  #feed the pitch and duration to the func$
+      time.sleep(duration[x] *0.5)
+      x+=1
+
+  elif(tune==4):
+    pitches=[1047, 988,659]
+    duration=[0.1,0.1,0.2]
+    for p in pitches:
+      self.buzz(p, duration[x])  #feed the pitch and duration to the func$
+      time.sleep(duration[x] *0.5)
+      x+=1
+
+  elif(tune==5):
+    pitches=[1047, 988,523]
+    duration=[0.1,0.1,0.2]
+    for p in pitches:
+      self.buzz(p, duration[x])  #feed the pitch and duration to the func$
+      time.sleep(duration[x] *0.5)
+      x+=1
+  '''
+
+if __name__ == "__main__":
+  #a = input("Enter Tune number 1-5:")
+  a = 1
+  buzzer = Buzzer()
+  buzzer.play(int(a))
