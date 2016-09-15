22f584f91679c7d34437bb5f59db684a4b03a069
wwj718 <wuwenjie718@gmail.com>
2016-09-15 10:58:04 +0800

    完成say客户端

---
+++ pc_client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import requests
+import get_hostname
+pc_ip = get_hostname.get_pc_ip('wwj-air.local')
+server_base_url = 'http://{}:5000'.format(pc_ip)
+def study():
+    requests.get(server_base_url+"/study")
+def sox():
+    requests.get(server_base_url+"/sox")
+def play():
+    requests.get(server_base_url+"/play")
+def say(content):
+    url = server_base_url+"/say"
+    payload = {"content":content}
+    requests.get(url,params=payload)
+
