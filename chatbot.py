2a11486c78d5c9dd4073e416bc441e2815845d79
wwj <wuwenjie718@gmail.com>
2016-09-24 23:37:00 +0800

    完成chatbot训练功能

--- chatbot.py
+++ chatbot.py
@@ -5,8 +5,11 @@
 #https://github.com/wwj718/wechat_bot/
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.training.trainers import ListTrainer
+
 deepThought = ChatBot("deepThought")
-deepThought.set_trainer(ChatterBotCorpusTrainer)
+deepThought.set_trainer(ListTrainer)
+#deepThought.set_trainer(ChatterBotCorpusTrainer)
 # 使用中文语料库训练它
 # 只需要训练一次，不需要每次启动进程都训练，训练结果默认存到本地`./database.db`,之后启动进程会使用这个数据库
 
@@ -30,7 +33,6 @@ def train(corpus):
     '''
 
     deepThought.train(corpus)
-    pass
 
 
 def main():
@@ -38,6 +40,9 @@ def main():
     pass
 
 if __name__ == '__main__':
-    main()
+    #main()
+    corpus=["嗳，渡边君，真喜欢我?","那还用说?"]
+    train(corpus)
+    print(deepThought.get_response('渡边君'))
 
 
