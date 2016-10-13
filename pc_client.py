41789475357f7f8971b3f2d2d4f73ce4b09f7fa4
wwj718 <wuwenjie718@gmail.com>
2016-10-13 10:05:09 +0000

    完成录音播音的client

--- pc_client.py
+++ pc_client.py
@@ -7,18 +7,12 @@ pc_ip = get_hostname.get_pc_ip('wwj-air.local')
 server_base_url = 'http://{}:5000'.format(pc_ip)
 def study():
     requests.get(server_base_url+"/study")
-def sox():
-    requests.get(server_base_url+"/sox")
+def sox(delay=3):
+    url = "{}/sox?delay={}".format(server_base_url,delay)
+    requests.get(url)
 def play():
     requests.get(server_base_url+"/play")
 def say(content):
     url = server_base_url+"/say"
     payload = {"content":content}
     requests.get(url,params=payload)
-def open_ai_talk(content):
-    url = server_base_url+"/openbot"
-    payload = {"content":content}
-    response = requests.get(url,params=payload)
-    print(response.json()) #js unicode
-
-#open_ai_talk(u'早啊')
