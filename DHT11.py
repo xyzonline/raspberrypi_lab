736e38a512f3d605c6ae2902f8c262f920755ca6
wwj718 <wuwenjie718@gmail.com>
2016-10-26 03:41:58 +0000

    添加gotty服务，替代Ajenti，使温度获取函数不依赖c库

--- DHT11.py
+++ DHT11.py
@@ -1,93 +1,68 @@
 #!/usr/bin/env python
 # encoding: utf-8
+#!/usr/bin/python
+import RPi.GPIO as GPIO
 import time
-import wiringpi as gpio
-import wiringpi
-owpin=8     #第8脚为1-wire脚
-runned = 0
-
-# http://www.cnblogs.com/yafengabc/p/5100741.html
-
-def getval(owpin):
-    global runned
-    tl=[]  #存放每个数据位的时间
-    tb=[]  #存放数据位
-    if runned == 0:
-        gpio.wiringPiSetup() #初始化wiringpi库 ,只初始化一次 http://stackoverflow.com/questions/29198312/error-on-wiringpi
-    gpio.pinMode(owpin,1)  #设置针脚为输出状态
-    gpio.digitalWrite(owpin,1) #输出高电平
-    gpio.delay(1)
-    gpio.digitalWrite(owpin,0) #拉低20ms开始指令
-    gpio.delay(25)
-    gpio.digitalWrite(owpin,1) #抬高20-40us
-    gpio.delayMicroseconds(20)
-    gpio.pinMode(owpin,0)     #设针脚为输入状态
-    while(gpio.digitalRead(owpin)==1): pass #等待DHT11拉低管脚
-
-    for i in range(45):   #测试每个数据周期的时间（包括40bit数据加一个发送开始标志
-        tc=gpio.micros()  #记下当前us数（从初始化开始算起，必要时重新初始化）
-        '''
-        一个数据周期，包括一个低电平，一个高电平，从DHT11第一次拉低信号线开始
-        到DHT11发送最后一个50us的低电平结束（然后被拉高，一直维持高电平，所以
-        最后的完成标志是一直为高，超过500ms）
-        '''
-        while(gpio.digitalRead(owpin)==0):pass
-        while(gpio.digitalRead(owpin)==1):
-            if gpio.micros()-tc>500: #如果超过500ms就结束了
-                break
-        if gpio.micros()-tc>500:   #跳出整个循环
-            break
-        tl.append(gpio.micros()-tc) #记录每个周期时间的us数，存到tl这个列表
-
-#    print(tl)      #反注释后可打印时间列表
-    tl=tl[1:]       #去掉第一项，剩下40个数据位
-    for i in tl:
-        if i>100:  #若数据位为1，时间为50us低电平+70us高电平=120us
-            tb.append(1)
-        else:
-            tb.append(0) #若数据位为0，时间为50us低电平+25us高电平=75us
-                                #这里取大于100us就为1
-#    print(tb)      #反注释可查看每一位状态
-    runned+=1
-    return tb
-
-def GetResult(output="temperature",owpin=owpin):
-    for i in range(10):
-        SH=0;SL=0;TH=0;TL=0;C=0
-        result=getval(owpin)
-#        print(len(result))
-        if len(result)==40:
-            for i in range(8):
-                #计算每一位的状态，每个字8位，以此为湿度整数，湿度小数，温度整数，温度小数，校验和
-                SH*=2;SH+=result[i]
-                SL*=2;SL+=result[i+8]
-                TH*=2;TH+=result[i+16]
-                TL*=2;TL+=result[i+24]
-                C*=2;C+=result[i+32]
-            if ((SH+SL+TH+TL)%256)==C and C!=0:
-                break
-            else:
-                print("Read Sucess,But checksum error! retrying")
-        else:
-            print("Read failer! Retrying")
-        gpio.delay(200)
-    #return SH,SL,TH,TL
-    if output == "humidity":
-        return SH
+def get_temperature_humidity():
+    channel =4 #GPIO4
+    data = []
+    j = 0
+    GPIO.setmode(GPIO.BCM)
+    time.sleep(1)
+    GPIO.setup(channel, GPIO.OUT)
+    GPIO.output(channel, GPIO.LOW)
+    time.sleep(0.02)
+    GPIO.output(channel, GPIO.HIGH)
+    GPIO.setup(channel, GPIO.IN)
+    while GPIO.input(channel) == GPIO.LOW:
+      continue
+    while GPIO.input(channel) == GPIO.HIGH:
+      continue
+    while j < 40:
+      k = 0
+      while GPIO.input(channel) == GPIO.LOW:
+        continue
+      while GPIO.input(channel) == GPIO.HIGH:
+        k += 1
+        if k > 100:
+          break
+      if k < 8:
+        data.append(0)
+      else:
+        data.append(1)
+      j += 1
+    print "sensor is working."
+    print data
+    humidity_bit = data[0:8]
+    humidity_point_bit = data[8:16]
+    temperature_bit = data[16:24]
+    temperature_point_bit = data[24:32]
+    check_bit = data[32:40]
+    humidity = 0
+    humidity_point = 0
+    temperature = 0
+    temperature_point = 0
+    check = 0
+    for i in range(8):
+      humidity += humidity_bit[i] * 2 ** (7-i)
+      humidity_point += humidity_point_bit[i] * 2 ** (7-i)
+      temperature += temperature_bit[i] * 2 ** (7-i)
+      temperature_point += temperature_point_bit[i] * 2 ** (7-i)
+      check += check_bit[i] * 2 ** (7-i)
+    tmp = humidity + humidity_point + temperature + temperature_point
+    if check == tmp:
+      print "temperature :", temperature, "*C, humidity :", humidity, "%"
+      return (temperature,humidity)
     else:
-        return  TH
-#程序应该有问题
-'''
-while 1:
-  try:
-    SH,SL,TH,TL=GetResult(owpin)
-    print u"湿度:",SH,SL,u"温度:",TH,TL #(SH,TH) #温度 湿度
-    #湿度: 63 0 温度: 23 0
-  except:
-    pass
-    #SH,SL,TH,TL=GetResult(owpin)
-  time.sleep(1)
-'''
+      print "wrong"
+      print "temperature :", temperature, "*C, humidity :", humidity, "% check :", check, ", tmp :", tmp
+      return "wrong"
+    GPIO.cleanup()
+def GetResult():
+    while 1:
+        result = get_temperature_humidity()
+        if result != "wrong":
+            return result
+
 if __name__=="__main__":
-    TH = GetResult()
-    print u"温度:",TH
+    GetResult()
