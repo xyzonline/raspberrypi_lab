6d40ffa930cd8cc3b1df242676fb85e18e446816
wwj <wuwenjie718@gmail.com>
2016-11-11 18:54:11 +0800

    add local settings

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # encoding: utf-8
-
 '''
 配置文件
 
@@ -9,11 +8,25 @@
 
 # 设置开关（feature）
 '''
+import os
+from os.path import expanduser
+import yaml
+
+#----------- <.pi_local.py> 隐私项目,参考：https://github.com/wwj718/ibot/blob/master/ibot/ibot.py
+# access_token turing_key  mail_host mail_user mail_pass
+home = expanduser("~")
+with open(os.path.join(home,".pi_local.yml")) as f:
+    pi_local_config = yaml.load(f)
+    #print pi_local_config
+    #print pi_local_config.get("haha") # None
+
 
 
 #------------------<flask app settings>
 APP_KEY = "test" # http请求需要携带的密码
 CODE_FILE = './codetest.py' # 树莓派是用户的，假设其代码可信
+# </flask app settings>
+
 #------------------<网络服务>  注意key的安全
 # for 百度语音
 access_token = "xxx"
@@ -30,8 +43,10 @@ mail_host="smtp.qq.com"  #设置服务器
 mail_user="pi@qq.com"    #用户名
 mail_pass="xxx"   #密码
 
-#------------------</网络服务>
+#</网络服务>
+#</.pi.local.py>
 
 
 #------------------<机器状态>
 ONLINE = False # 默认单机转态
+# </机器状态>
